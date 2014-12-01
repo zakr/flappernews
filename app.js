7e1821f2c31749b0c8bd2c3b6bd1930ebce4adca
Zak Rome <zak.rome@yahoo.com>
2014-12-01 14:49:41 -0700

    Adding initial style and a service

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.factory('posts', [function(){
+	var o = {
+		posts: []
+	}
+	return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-	$scope.test = '';
-	$scope.posts = [
-		{title: 'post 1', link: 'http://www.google.com', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', link: 'http://www.google.com', upvotes: 15},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', link: 'http://www.google.com', upvotes: 4}
-	];
+'posts',
+function($scope, posts){
+	// $scope.test = '';
+	// $scope.posts = [
+	// 	{title: 'post 1', link: 'http://www.google.com', upvotes: 5},
+	// 	{title: 'post 2', upvotes: 2},
+	// 	{title: 'post 3', link: 'http://www.google.com', upvotes: 15},
+	// 	{title: 'post 4', upvotes: 9},
+	// 	{title: 'post 5', link: 'http://www.google.com', upvotes: 4}
+	// ];
+	$scope.posts = posts.posts;
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === ''){
 			return;
 		}
 		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
 		$scope.title = '';
-	}
+	};
 	$scope.incrementUpvotes = function(post){
 		post.upvotes += 1;
-	}
+	};
+	$scope.decrementUpvotes = function(post){
+		post.upvotes -= 1;
+	};
 }]);
