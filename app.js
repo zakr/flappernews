b0c023fd42b960d89c38ea10980e8fe320c6d130
Zak Rome <zak.rome@yahoo.com>
2014-12-01 12:36:29 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+angular.module('flapperNews', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+	$scope.test = 'Hello World';
+	$scope.posts = [
+		'post 1',
+		'post 2',
+		'post 3',
+		'post 4',
+		'post 5'
+	];
+}]);
