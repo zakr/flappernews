20ab81cc7792262b6ede91f9d382930b7bbff0b7
Zak Rome <zak.rome@yahoo.com>
2014-12-01 12:55:27 -0700

    Updated initial code

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-	$scope.test = 'Hello World';
 	$scope.posts = [
-		'post 1',
-		'post 2',
-		'post 3',
-		'post 4',
-		'post 5'
+		{title: 'post 1', link: 'http://www.google.com', upvotes: 5},
+		{title: 'post 2', upvotes: 2},
+		{title: 'post 3', link: 'http://www.google.com', upvotes: 15},
+		{title: 'post 4', upvotes: 9},
+		{title: 'post 5', link: 'http://www.google.com', upvotes: 4}
 	];
+	$scope.addPost = function(){
+		if(!$scope.title || $scope.title === ''){
+			return;
+		}
+		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+		$scope.title = '';
+	}
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	}
 }]);
