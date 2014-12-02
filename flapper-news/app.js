338a11a6f9cfc6bcf4dfc8f435027dec3d41cbe3
Zak Rome <zak.rome@yahoo.com>
2014-12-02 13:24:49 -0700

    Finished tutorial and did some minor customizations

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -3,6 +3,12 @@
  * Module dependencies.
  */
 
+ // Mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
+require('./models/Posts');
+require('./models/Comments');
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -32,4 +38,127 @@ app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+// ROUTES
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+app.get('/posts', function(req, res, next){
+	Post.find(function(err, posts){
+		if(err){
+			return next(err);
+		}
+		res.json(posts);
+	});
+});
+
+app.post('/posts', function(req, res, next){
+	var post = new Post(req.body);
+	post.save(function(err, post){
+		if(err){
+			return next(err);
+		}
+		res.json(post);
+	});
+});
+
+app.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+	query.exec(function(err, post){
+		if(err){
+			return next(err);
+		}
+		if(!post){
+			return next(new Error("Can't Find Post"));
+		}
+		req.post = post;
+		return next();
+	})
+});
+
+app.get('/posts/:post', function(req, res){
+	req.post.populate('comments', function(err, post){
+		res.json(req.post);
+	});
+});
+
+app.put('/posts/:post/upvote', function(req, res, next){
+	req.post.upvote(function(err, post){
+		if(err){
+			return next(err);
+		}
+		res.json(post);
+	});
+});
+
+app.put('/posts/:post/downvote', function(req, res, next){
+	req.post.downvote(function(err, post){
+		if(err){
+			return next(err);
+		}
+		res.json(post);
+	});
+});
+
+app.get('/posts/:post/comments', function(req, res, next){
+	Comment.find(function(err, comments){
+		if(err){
+			return next(err);
+		}
+		res.json(comments);
+	});
+});
+
+app.post('/posts/:post/comments', function(req, res, next){
+	var comment = new Comment(req.body);
+	comment.post = req.post;
+	comment.save(function(err, comment){
+		if(err){
+			return next(err);
+		}
+		req.post.comments.push(comment);
+		req.post.save(function(err, post){
+			if(err){
+				return next(err);
+			}
+			res.json(comment);
+		});
+	});
+});
+
+app.param('comment', function(req, res, next, id){
+	var query = Comment.findById(id);
+	query.exec(function(err, comment){
+		if(err){
+			return next(err);
+		}
+		if(!comment){
+			return next(new Error("Can't Find Comment"));
+		}
+		req.comment = comment;
+		return next();
+	})
+});
+
+app.get('/posts/:post/comments/:comment', function(req, res){
+	res.json(req.comment);
+});
+
+app.put('/posts/:post/comments/:comment/upvote', function(req, res, next){
+	req.comment.upvote(function(err, comment){
+		if(err){
+			return next(err);
+		}
+		res.json(comment);
+	});
+});
+
+app.put('/posts/:post/comments/:comment/downvote', function(req, res, next){
+	req.comment.downvote(function(err, comment){
+		if(err){
+			return next(err);
+		}
+		res.json(comment);
+	});
 });
