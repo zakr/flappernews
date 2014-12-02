8a6ccd8aa43b15de80a45e61a627f94d5e774d84
Zak Rome <zak.rome@yahoo.com>
2014-12-01 17:07:31 -0700

    Adding pages for posts with comments

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '/home.html',
+			controller: 'MainCtrl'
+		})
+		.state('posts', {
+			url: '/posts/{id}',
+			templateUrl: '/posts.html',
+			controller: 'PostsCtrl'
+		})
+	$urlRouterProvider.otherwise('home');
+}])
 .factory('posts', [function(){
 	var o = {
-		posts: []
+		posts: [
+			{title: 'post 1', link: 'http://www.google.com', upvotes: 5}
+		]
 	}
 	return o;
 }])
@@ -22,7 +41,16 @@ function($scope, posts){
 		if(!$scope.title || $scope.title === ''){
 			return;
 		}
-		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+		//$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+		$scope.posts.push({
+		  title: $scope.title,
+		  link: $scope.link,
+		  upvotes: 0,
+		  comments: [
+		    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+		    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		  ]
+		});
 		$scope.title = '';
 	};
 	$scope.incrementUpvotes = function(post){
@@ -30,5 +58,23 @@ function($scope, posts){
 	};
 	$scope.decrementUpvotes = function(post){
 		post.upvotes -= 1;
+	};
+}])
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+	$scope.addComment = function(){
+		if($scope.body === ''){
+			return;
+		}
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
 	};
 }]);
